9652e7f7f8fdbe59a0665bd1f9ab91407d3b2af6
Xeragus <bobansugareski@gmail.com>
2022-06-22 22:05:40 +0200

    Add code for lecture 5

--- lecture-5/controllers/index.js
+++ lecture-5/controllers/index.js
@@ -1,6 +1,24 @@
 module.exports = {
     index: (req, res) => {
-        res.render('index', { title: 'Balkan Express', subtitle: 'Hehehe' });
+        const users = [
+            {
+                name: 'Boban',
+                lastname: 'Sugareski',
+                ssn: 1234
+            },
+            {
+                name: 'Jovan',
+                lastname: 'Popov',
+                ssn: 445656
+            },
+            {
+                name: 'Goran',
+                lastname: 'Zoran',
+                ssn: 2345
+            }
+        ];
+
+        res.render('index', { title: 'Balkan Express', subtitle: 'Hehehe', users: users });
     },
     hehe: (req, res) => {
         res.render('hehe', { title: 'Hehe Express', subtitle: 'Hehe OG' });
