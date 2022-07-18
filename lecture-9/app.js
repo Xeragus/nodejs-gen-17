c2f9fa577d8145446b5ca141de5af579fe2c7dd0
Xeragus <bobansugareski@gmail.com>
2022-07-18 20:18:19 +0200

    Add code for lecture 9, databases discussion

---
+++ lecture-9/app.js
@@ -0,0 +1,39 @@
+// social net
+// users, posts, comments, likes
+
+// collection: a set of similar information
+// entity, attributes
+
+// entity: User, attributes: first name, last name, email, ssn
+// entity: Post, attributes: title, content
+// entity: Comment, attributes: content
+
+collection: users
+[
+    {
+        id: 1,
+        name: 'Boban'
+    },
+    {
+        id: 2,
+        name: 'Mitko'
+    }
+    // ...
+]
+
+collections: posts
+[
+    {
+        id: 1,
+        title: 'T-Shirt',
+        content: 'Red shirt hehe',
+        user_id: 1 
+    },
+    {
+        id: 2,
+        title: 'T-Shirt',
+        content: 'Red shirt hehe hhihihihihi',
+        user_id: 444
+    }
+    // ...
+]
